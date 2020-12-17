cd9ad07bf322c394f092918d2ffa24becf8f528e
swolepenguin <ashtonm223@gmail.com>
2020-12-17 12:49:24 -0500

    added config and passport

---
+++ config/passport.js
@@ -0,0 +1,21 @@
+const passport = require('passport')
+const { ExtractJwt } = require('passport-jwt')
+
+require('dotenv').config
+
+//passprt strategy for authenticating with a JSON web token
+//this allows us to authenticate endpoints using a token 
+const JwtStrategy = require('passport-jwt').Strategy
+const Extract = require('passport-jwt').ExtractJwt
+
+const options = {}
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+//key is inside of our Environment
+options. secretOrKey = process.env.Jwt_SECRET 
+
+module.exports = (passport) =>{
+    passport.use(new JwtStrategy(options, (jwt_payload, done)=>{
+        //have a user that were going to find by the ied in the paylod
+        // when we get a user back we will check the database
+    }))
+}
---
+++ models/server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+//mongo connection here
+mongoose.connect(process.eventNames.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
+
+//mongoose connection object 
+const db = mongoose.connection
+
+//setup an event listenermthat will fire once the connection opens for the database 
+//log to the terminal what host and port we are on
+db,once('open',()=>{
+    console.log(`connected to mongoDB at ${db.host}:${db.port}`)
+})
+
+db.on('error',(error)=>{
+    console.log(`database error\n ${error}`)
+})
+const User = require('./User')
+
+module.exports = User
