625d9994db4b699d2aea541a88ae5e8a447027a4
swolepenguin <ashtonm223@gmail.com>
2020-12-17 13:09:14 -0500

    connect to db

--- models/index.js
+++ models/index.js
@@ -1,24 +1,28 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 //mongo connection here
-mongoose.connect(process.eventNames.MONGO_URI,{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true,
     useUnifiedTopology: true,
     useFindAndModify: true
 })
 
+console.log(process.env)
+
 //mongoose connection object 
 const db = mongoose.connection
+console.log(db)
 
 //setup an event listenermthat will fire once the connection opens for the database 
 //log to the terminal what host and port we are on
-db,once('open',()=>{
+db.once('open',()=>{
     console.log(`connected to mongoDB at ${db.host}:${db.port}`)
 })
 
 db.on('error',(error)=>{
     console.log(`database error\n ${error}`)
 })
-const User = require('./User')
+// const User = require('./User')
 
-module.exports = User
+// module.exports = User
